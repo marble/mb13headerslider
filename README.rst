d63cec00f797d0c9ddb1e370fb665ed4e706b9ea
Martin Bless <martin@mbless.de>
2015-06-05 17:21:11 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,17 @@
+
+
+======================================
+TYPO3 CMS Extension 'mb13headerslider'
+======================================
+
+:Author: Martin Bless <martin@mbless.de>
+:Requires: Developed for TYPO3 >= 6.2
+
+
+What does it do?
+================
+
+- Implements a slider for header images
+- Images are taken from the media field of the pages
+- Uses http://bxslider.com/
+
---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "mb13headerslider"
+ *
+ * Manual updates:
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'A TypoScript headerslider',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Martin Bless',
+	'author_email' => 'martin@mbless.de',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => '0',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
